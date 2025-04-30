13a6de632a738ee34370ef31e5b12da5fc9d4fc5
Abhay Singh <sinnghabhay80@gmail.com>
2025-04-30 21:15:10 +0530

    update config-loader to handle env variables, and api-reader to use custom config utility

--- ingestion/spark_ingestion_job/utils/config_loader.py
+++ ingestion/spark_ingestion_job/utils/config_loader.py
@@ -1,15 +1,43 @@
 import yaml
+import os
+import re
 from pathlib import Path
 from typing import Dict
+from dotenv import load_dotenv
+
+# Load .env file once at module level
+load_dotenv()
+
+env_var_pattern = re.compile(r"\$\{([A-Z0-9_]+)\}")
+
+def _interpolate_env_vars(value: str) -> str:
+    match = env_var_pattern.findall(value)
+    for var in match:
+        env_value = os.environ.get(var)
+        if env_value is None:
+            raise EnvironmentError(f"Environment variable {var} is not set.")
+        value = value.replace(f"${{{var}}}", env_value)
+    return value
+
+
+def _expand_env_in_dict(config_dict):
+    for key, value in config_dict.items():
+        if isinstance(value, dict):
+            _expand_env_in_dict(value)
+        elif isinstance(value, str) and "${" in value:
+            config_dict[key] = _interpolate_env_vars(value)
+    return config_dict
 
 
 def load_config(config_path: str = "config/config.yaml") -> Dict:
     """
-    Loads YAML configuration from the given path.
+        Loads YAML configuration from the given path.
     """
     config_file = Path(config_path)
     if not config_file.exists():
         raise FileNotFoundError(f"Config file not found at {config_path}")
 
     with open(config_file, "r") as f:
-        return yaml.safe_load(f)
+        raw_config = yaml.safe_load(f)
+
+    return _expand_env_in_dict(raw_config)
--- ingestion/spark_ingestion_job/utils/opensky_api_reader.py
+++ ingestion/spark_ingestion_job/utils/opensky_api_reader.py
@@ -1,11 +1,10 @@
 import requests
-import yaml
 import logging
-from pathlib import Path
 from typing import List, Dict, Optional
+from ingestion.spark_ingestion_job.utils.config_loader import load_config
 from tenacity import retry, wait_fixed, stop_after_attempt, retry_if_exception_type
 
-# Configuring structured logging
+# Logging setup
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
@@ -14,37 +13,17 @@ logger = logging.getLogger("OpenSkyAPIClient")
 
 
 class OpenSkyAPIClient:
-    def __init__(self, config_path: str = "../config/api_keys.yaml"):
+    def __init__(self, config_path: str = "../config/config.yaml"):
         """
-        Initializes the OpenSky API client and attempts to load credentials.
+        Initializes the OpenSky API client with credentials from YAML config.
         """
+        self.config = load_config(config_path)
         self.base_url = "https://opensky-network.org/api/states/all"
-        self.username, self.password = self._load_credentials(config_path)
+        self.username = self.config["opensky"].get("username")
+        self.password = self.config["opensky"].get("password")
 
-    def _load_credentials(self, path: str) -> (Optional[str], Optional[str]):
-        """
-        Load API credentials from a YAML config file.
-        """
-        try:
-            config_file = Path(path)
-            if config_file.exists():
-                with open(config_file, "r") as f:
-                    cfg = yaml.safe_load(f)
-                    username = cfg["opensky"].get("username")
-                    password = cfg["opensky"].get("password")
-                    if username and password:
-                        logger.info("Loaded OpenSky API credentials.")
-                    else:
-                        logger.warning("Username/password missing in YAML, using unauthenticated mode.")
-                    return username, password
-            else:
-                logger.warning("API key config not found. Using unauthenticated mode.")
-                return None, None
-        except Exception as e:
-            logger.error(f"Error reading credentials: {e}")
-            return None, None
-
-    @retry(wait=wait_fixed(10), stop=stop_after_attempt(3), retry=retry_if_exception_type(requests.RequestException))
+    @retry(wait=wait_fixed(10), stop=stop_after_attempt(3),
+           retry=retry_if_exception_type(requests.RequestException))
     def fetch_states(self) -> Optional[List[Dict]]:
         """
         Fetch live aircraft state data from OpenSky API.
