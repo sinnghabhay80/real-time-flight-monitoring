6252f86f9eb834ba1b0db565df95307b58dfaaaf
sinnghabhay80 <sinnghabhay80@gmail.com>
2025-05-11 23:39:31 +0530

    add DAG for Airflow orchestration

--- orchestration/dags/flight-monitoring-dag.py
+++ orchestration/dags/flight-monitoring-dag.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.utils.dates import days_ago
-from datetime import timedelta
+from datetime import datetime, timedelta
+from utils.config_loader import load_config
 
 #Default Args
 default_args = {
@@ -13,35 +13,37 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
+config = load_config()
+bronze_dag_process = config["paths"]["bronze_dag_script"]
+silver_dag_process = config["paths"]["silver_dag_script"]
+gold_dag_process = config["paths"]["gold_level_script"]
+
+
 #DAG definition
-dag = DAG(
+with DAG(
     'flight-monitoring',
     default_args=default_args,
     description='Orchestrate Bronze -> Silver -> Gold',
-    schedule_interval = None,
-    start_date=days_ago(1),
+    schedule=None,
+    start_date=(datetime.now() - timedelta(days=1)),
     catchup=False,
     tags=['flight-monitoring']
-)
-
-#Define tasks
-bronze_process = BashOperator(
+) as dag:
+    #Define tasks
+    bronze_process = BashOperator(
     task_id='bronze_process',
-    bash_command = "",
-    dag=dag
-)
+    bash_command = f"python {bronze_dag_process}",
+    )
 
-silver_process = BashOperator(
+    silver_process = BashOperator(
     task_id='silver_process',
-    bash_command = "",
-    dag=dag
-)
+    bash_command = f"python {silver_dag_process}",
+    )
 
-gold_process = BashOperator(
+    gold_process = BashOperator(
     task_id='gold_process',
-    bash_command = "",
-    dag=dag
-)
+    bash_command = f"python {gold_dag_process}",
+    )
 
-#Task Lineage
-bronze_process >> silver_process >> gold_process
+    #Task Lineage
+    bronze_process >> silver_process >> gold_process
