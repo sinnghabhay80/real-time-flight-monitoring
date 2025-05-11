1b6a2812e6c5c06bd0bda774ae536836015dfbe1
sinnghabhay80 <sinnghabhay80@gmail.com>
2025-05-11 23:39:59 +0530

    add scripts for bronze and silver process life checks

--- orchestration/scripts/silver_process.py
+++ orchestration/scripts/silver_process.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 import logging
 from utils.process_life_check import process_alive_or_not
+from utils.config_loader import load_config
 
 # Logging
 logging.basicConfig(
@@ -12,8 +13,10 @@ logger = logging.getLogger("BronzeDAG")
 
 
 def check_or_start_bronze():
-    script_name = os.path.basename(__file__)
-    script_path = os.path.realpath(__file__)
+    config = load_config()
+    silver_script_path = config["paths"]["silver_level_script"]
+    script_name = os.path.basename(silver_script_path)
+    script_path = os.path.realpath(silver_script_path)
 
     process_life = process_alive_or_not(script_name)
 
