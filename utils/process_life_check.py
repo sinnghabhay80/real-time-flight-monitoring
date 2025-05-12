db655198a97551267f24bfaf81e2033cfa7a73db
sinnghabhay80 <sinnghabhay80@gmail.com>
2025-05-13 00:13:10 +0530

    util for checking if a process is alive by script path

--- utils/process_life_check.py
+++ utils/process_life_check.py
@@ -9,10 +9,12 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def process_alive_or_not(script_name: str) -> bool:
-    for process in psutil.process_iter(['pid', 'name', 'cmdline']):
+def process_alive_or_not(script_path: str) -> bool:
+    current_processes = list(psutil.process_iter(['pid', 'name', 'cmdline']))
+    for process in current_processes:
         try:
-            if script_name in process.cmdline:
+            cmdline = process.info.get('cmdline')
+            if cmdline and script_path in cmdline:
                 return True
 
         except (psutil.NoSuchProcess, psutil.AccessDenied):
